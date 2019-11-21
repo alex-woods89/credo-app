3b3a99c82894deed19b2d6c6db4d7a011ee80f8f
Alex Woods <alex.woods89@hotmail.co.uk>
2019-11-21 12:37:22 +0000

    navbar

--- credo_client/src/Main.js
+++ credo_client/src/Main.js
@@ -8,7 +8,23 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class Main extends Component{
 
+render(){
+    return (
+        <Router>
+        <Fragment>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/pipes" component={PipeContainer} />
+            <Route path="/users" component={UserContainer} />
+            <Route path="/valves" component={ValveContainer} />
+            
+          </Switch>
 
+        </Fragment>
+      </Router>
+    )
+}
 
 
 }
