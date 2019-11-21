cc89f9a8907ae1c6697528c96178c28b83dfa785
Alex Woods <alex.woods89@hotmail.co.uk>
2019-11-21 11:38:57 +0000

    started navbar

---
+++ credo_client/src/Main.js
@@ -0,0 +1,8 @@
+import React, {Component, Fragment } from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import PipeContainer from './containers/pipeContainer';
+import UserContainer from './containers/userContainer';
+import ValveContainer from './containers/valveContainer';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
